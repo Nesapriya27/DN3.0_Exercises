d9990cb11b5bd830ff57cbc04d8e4d51ef183420
Nesapriya27 <115575778+Nesapriya27@users.noreply.github.com>
2024-07-30 22:26:51 +0530

    Create Abstract.java

---
+++ Week_1_Exercises/DesignPatternsAndPrinciples/FactoryMethodPattern/Abstract.java
@@ -0,0 +1,19 @@
+// Abstract factory class for creating documents
+public abstract class DocumentFactory {
+    public abstract Document createDocument();
+
+    public void openDocument() {
+        Document doc = createDocument();
+        doc.open();
+    }
+
+    public void saveDocument() {
+        Document doc = createDocument();
+        doc.save();
+    }
+
+    public void closeDocument() {
+        Document doc = createDocument();
+        doc.close();
+    }
+}
