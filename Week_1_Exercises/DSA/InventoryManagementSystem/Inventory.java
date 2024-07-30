cb051549d45ceea8ac04ce3044038a7c9d70161f
Nesapriya27 <115575778+Nesapriya27@users.noreply.github.com>
2024-07-30 21:09:41 +0530

    Create Inventory.java

---
+++ Week_1_Exercises/DSA/InventoryManagementSystem/Inventory.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class InventoryManagementSystem {
+    private Map<String, Product> inventory;
+
+    public InventoryManagementSystem() {
+        inventory = new HashMap<>();
+    }
+    public void addProduct(Product product) {
+        inventory.put(product.getProductId(), product);
+    }
+
+    public void updateProduct(String productId, Product updatedProduct) {
+        if (inventory.containsKey(productId)) {
+            inventory.put(productId, updatedProduct);
+        } else {
+            System.out.println("Product not found.");
+        }
+    }
+    public void deleteProduct(String productId) {
+        if (inventory.containsKey(productId)) {
+            inventory.remove(productId);
+        } else {
+            System.out.println("Product not found.");
+        }
+    }
+    public void displayInventory() {
+        for (Product product : inventory.values()) {
+            System.out.println(product);
+        }
+    }
+}
