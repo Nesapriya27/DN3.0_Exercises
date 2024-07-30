81f4f5a74e72496c4cbad68484a3c9c120b922c4
Nesapriya27 <115575778+Nesapriya27@users.noreply.github.com>
2024-07-30 22:29:23 +0530

    Create Product.java

---
+++ Week_1_Exercises/DesignPatternsAndPrinciples/BuilderPattern/Product.java
@@ -0,0 +1,66 @@
+public class Computer {
+    private String CPU;
+    private String RAM;
+    private String storage;
+    private String GPU;
+    private String powerSupply;
+    private String motherboard;
+    private Computer(Builder builder) {
+        this.CPU = builder.CPU;
+        this.RAM = builder.RAM;
+        this.storage = builder.storage;
+        this.GPU = builder.GPU;
+        this.powerSupply = builder.powerSupply;
+        this.motherboard = builder.motherboard;
+    }
+
+    // Getters for the attributes
+    public String getCPU() { return CPU; }
+    public String getRAM() { return RAM; }
+    public String getStorage() { return storage; }
+    public String getGPU() { return GPU; }
+    public String getPowerSupply() { return powerSupply; }
+    public String getMotherboard() { return motherboard; }
+
+    // Static nested Builder class
+    public static class Builder {
+        private String CPU;
+        private String RAM;
+        private String storage;
+        private String GPU;
+        private String powerSupply;
+        private String motherboard;
+        public Builder setCPU(String CPU) {
+            this.CPU = CPU;
+            return this;
+        }
+
+        public Builder setRAM(String RAM) {
+            this.RAM = RAM;
+            return this;
+        }
+
+        public Builder setStorage(String storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        public Builder setGPU(String GPU) {
+            this.GPU = GPU;
+            return this;
+        }
+
+        public Builder setPowerSupply(String powerSupply) {
+            this.powerSupply = powerSupply;
+            return this;
+        }
+
+        public Builder setMotherboard(String motherboard) {
+            this.motherboard = motherboard;
+            return this;
+        }
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
