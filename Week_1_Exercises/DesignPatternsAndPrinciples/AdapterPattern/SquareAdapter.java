1fede9003395770d506c4f24513a3bddfce2e894
Nesapriya27 <115575778+Nesapriya27@users.noreply.github.com>
2024-07-30 22:35:39 +0530

    Create SquareAdapter.java

---
+++ Week_1_Exercises/DesignPatternsAndPrinciples/AdapterPattern/SquareAdapter.java
@@ -0,0 +1,12 @@
+public class SquareAdapter implements PaymentProcessor {
+    private SquarePaymentGateway squarePaymentGateway;
+
+    public SquareAdapter(SquarePaymentGateway squarePaymentGateway) {
+        this.squarePaymentGateway = squarePaymentGateway;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        squarePaymentGateway.pay(amount);
+    }
+}
