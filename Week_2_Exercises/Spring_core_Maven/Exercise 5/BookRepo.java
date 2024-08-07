42cf2db345f225991815c382a3ba1c63f706cad7
Nesapriya27 <115575778+Nesapriya27@users.noreply.github.com>
2024-08-07 14:36:51 +0530

    Create BookRepo.java

---
+++ Week_2_Exercises/Spring_core_Maven/Exercise 5/BookRepo.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void findAll() {
+        System.out.println("Finding all books...");
+        // Simulated database access
+    }
+}
