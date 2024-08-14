1f586c923f65f5ae4f1565c54de35ac241ab1e1e
Nesapriya27 <115575778+Nesapriya27@users.noreply.github.com>
2024-08-14 22:18:48 +0530

    Create Controller.java

---
+++ Week_3_Exercises/Exercise 8/Controller.java
@@ -0,0 +1,21 @@
+package com.example.EmployeeManagementSystem.controller;
+
+import com.example.EmployeeManagementSystem.projection.EmployeeNameEmailProjection;
+import com.example.EmployeeManagementSystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees/projection")
+    public List<EmployeeNameEmailProjection> getEmployeeNameEmail() {
+        return employeeRepository.findAllProjectedBy();
+    }
+}
