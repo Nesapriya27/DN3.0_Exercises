86f61022fb37d4b2c187ace41ba91ebb50913bfe
Nesapriya27 <115575778+Nesapriya27@users.noreply.github.com>
2024-08-14 22:06:26 +0530

    Create EmployeeService.java

---
+++ Week_3_Exercises/Exercise 4/EmployeeService.java
@@ -0,0 +1,39 @@
+package com.example.EmployeeManagementSystem.service;
+
+import com.example.EmployeeManagementSystem.entity.Employee;
+import com.example.EmployeeManagementSystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
+    }
+
+    public Employee updateEmployee(Long id, Employee employeeDetails) {
+        Employee employee = getEmployeeById(id);
+        employee.setName(employeeDetails.getName());
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setDepartment(employeeDetails.getDepartment());
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
