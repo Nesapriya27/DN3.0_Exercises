e0f8fcd20fb4e93891287d00676bd13d10b19163
Nesapriya27 <115575778+Nesapriya27@users.noreply.github.com>
2024-08-14 22:11:09 +0530

    Create EmployeeController.java

---
+++ Week_3_Exercises/Exercise 6/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.example.EmployeeManagementSystem.controller;
+
+import com.example.EmployeeManagementSystem.entity.Employee;
+import com.example.EmployeeManagementSystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // Endpoint to get employees with pagination
+    @GetMapping("/employees")
+    public Page<Employee> getAllEmployees(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        return employeeRepository.findAll(pageable);
+    }
+}
